a6210dd368a51f47352ec0914da2b9db97e99ca2
James Bewley <james.e.bewley@gmail.com>
2022-01-20 19:33:06 -0700

    Initial commit

---
+++ src/main/java/com/example/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.example.hello;
+
+import java.util.Arrays;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }// END: main()
+
+}// END: class Hello
